c9797b166084196e24be5b78728cd8b6d5962035
anaymous <max997123@gmail.com>
2024-01-17 20:08:05 -0500

    fix second query and add a query to calculate count of america countries and pork production in 2019

--- queries.sql
+++ queries.sql
@@ -13,18 +13,31 @@ SELECT area, item, AVG(value) AS annual_production
 FROM crop_livestock_stats
 WHERE area = 'World'
 AND year BETWEEN 2000 AND 2021
-AND item = 'Fruits, Primary'
+AND item = 'Fruit Primary'
 AND element = 'Production'
 GROUP BY area, item;
 
 /* Filter the data to only countries in 
 Northern America, Central America, South America */
-SELECT 	s.*, 
-	c.country_group 
-FROM crop_livestock_stats s, 
-country_groups c
-WHERE c.country_group IN ('North America', 'South America', 'Central America') 
+SELECT *,country_group 
+FROM crop_livestock_stats
+LEFT JOIN country_groups
+ON country_group = area
+WHERE area IN ('Northern America', 'Central America', 'South America')
 LIMIT 1000;
+
+/* For Northern America, Central America, 
+and South America, calculate the number of
+countries and total production of "Meat, Poultry" in 2010. */
+
+SELECT area,
+COUNT(country), SUM(value)
+FROM crop_livestock_stats 
+RIGHT JOIN country_groups
+ON country_code = area_code
+WHERE area IN ('Northern America', 'Central America', 'South America')
+GROUP BY area;
+
 
 
 
